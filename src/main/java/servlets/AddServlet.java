1b4fe95d79f9a9be030fd73842620529f4fa7602
laneinline <44878400+laneinline@users.noreply.github.com>
2020-07-30 21:50:37 +0300

    Result now shown in display.jsp

--- src/main/java/servlets/AddServlet.java
+++ src/main/java/servlets/AddServlet.java
@@ -1,13 +1,29 @@
 package servlets;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import service.AddService;
 
 @Controller
 public class AddServlet {
 	@RequestMapping("/add")
-	public String add() {
-		return "display.jsp";
+	public ModelAndView add(HttpServletRequest request, HttpServletResponse response) {
+		
+		ModelAndView modelAndView = new ModelAndView();
+		int result;
+		AddService addService = new AddService();
+		
+		
+		result = addService.add(Integer.valueOf(request.getParameter("text1")), Integer.valueOf(request.getParameter("text2")));
+		
+		modelAndView.setViewName("display.jsp");
+		modelAndView.addObject("result",result);
+		return modelAndView;
 
 	}
 }
--- src/main/webapp/display.jsp
+++ src/main/webapp/display.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+    pageEncoding="UTF-8"  isELIgnored="false"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-display.jsp
+ This is your result: ${ result }
 </body>
 </html>
